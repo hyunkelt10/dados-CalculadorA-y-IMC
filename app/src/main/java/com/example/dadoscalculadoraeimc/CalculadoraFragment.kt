22d14ccb09327fe0667e7da97f3c4fb10e77e047
ignacio <hyunkelt10@gmail.com>
2022-07-21 19:21:04 +0200

    dados funcionando

--- app/src/main/java/com/example/dadoscalculadoraeimc/CalculadoraFragment.kt
+++ app/src/main/java/com/example/dadoscalculadoraeimc/CalculadoraFragment.kt
@@ -1,16 +1,27 @@
 package com.example.dadoscalculadoraeimc
 
 import android.os.Bundle
+import android.view.ContextMenu
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.textclassifier.TextClassifier.NO_OP
 import com.example.dadoscalculadoraeimc.databinding.FragmentCalculadoraBinding
+import kotlin.random.Random
 
 
 class CalculadoraFragment : Fragment() {
-   private var _b: FragmentCalculadoraBinding? = null
-   private val b get () = _b!!
+    private var _b: FragmentCalculadoraBinding? = null
+    private val b get () = _b!!
+
+
+
+
+
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +34,11 @@ class CalculadoraFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+
     }
+
+
+
 }
+
